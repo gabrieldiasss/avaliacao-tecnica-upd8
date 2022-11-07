cb1710b31c3c1446633e939a3e4965bad0d86f06
Wander Rodrigues <wander@writ.solutions>
2022-11-07 16:55:08 -0300

    feat(add): config project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AppRoutes from './routes';
+
+const App = () => {
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
