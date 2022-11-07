e5bfe2bae963677b6935f4043719bda82820bbe9
Wander Rodrigues <wander@writ.solutions>
2022-11-07 01:33:59 -0300

    feat(add): swagger setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const config = new DocumentBuilder()
+  .setTitle('Backend')
+  .setVersion('1.0.0')
+  .build();
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
+}
 bootstrap();
