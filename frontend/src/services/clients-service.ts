bc21576c2769eeb1d82f10079d09cc0ec675f254
Wander Rodrigues <wander@writ.solutions>
2022-11-07 16:53:43 -0300

    feat(add): clients service

---
+++ frontend/src/services/clients-service.ts
@@ -0,0 +1,47 @@
+import { api } from "../config/api"
+import { Client } from "../types/client.type"
+
+export interface ClientCreateDto {
+  cpf: string;
+  name: string;
+  birth: Date;
+  gender: 'MALE' | 'FEMALE';
+  address: string;
+  state: string;
+  city: string;
+}
+
+export interface ClientUpdateDto {
+  cpf?: string;
+  name?: string;
+  birth?: Date;
+  gender?: 'MALE' | 'FEMALE';
+  address?: string;
+  state?: string;
+  city?: string;
+}
+
+const { findAll, del, update, create, findOne } = {
+  findAll: async (): Promise<Client[]> => {
+    const { data } = await api.get<Client[]>('clients')
+    return data
+  },
+  del: async (id: number): Promise<Client> => {
+    const { data } = await api.delete<Client>(`clients/${id}`)
+    return data
+  },
+  create: async(dto: ClientCreateDto): Promise<Client> => {
+    const { data } = await api.post<Client>('clients', dto)
+    return data
+  },
+  update: async(id: number, dto: ClientUpdateDto): Promise<Client> => {
+    const { data } = await api.patch<Client>(`clients/${id}`, dto)
+    return data
+  },
+  findOne: async (id?: number): Promise<Client> => {
+    const { data } = await api.get<Client>(`clients/${id}`)
+    return data
+  }
+}
+
+export { findAll, del, update, create, findOne }
